ab1436a20b24a720ee8bdd01b9759ed369607197
James Erickson <hailfire91@gmail.com>
2020-02-08 21:49:12 -0600

    Initial commit.

---
+++ src/main/java/com/jce/jcepetclinic/JcePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jce.jcepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jce/jcepetclinic/JcePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jce.jcepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
